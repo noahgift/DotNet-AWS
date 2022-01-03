07e342f4247c25e8749b3897e079acba0344b16b
Noah Gift <noah.gift@gmail.com>
2022-01-03 15:03:47 -0500

    aws bucket lister

---
+++ chapters/chap1/AWSS3List/AWSS3List/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// To interact with AWS S3
+using Amazon.S3;
+using Amazon.S3.Model;
+
+// Create s3 client
+var s3Client = new AmazonS3Client();
+
+// Display Prompt
+Console.WriteLine("AWS S3 Bucket Lister " + Environment.NewLine);
+
+// Process API Calls Async List AWS Buckets
+var listResponse = await MyListBucketsAsync(s3Client);
+Console.WriteLine($"Number of buckets: {listResponse.Buckets.Count}");
+
+// Loop through the AWS buckets
+foreach (S3Bucket b in listResponse.Buckets)
+{
+    Console.WriteLine($"BucketName: {b.BucketName}");
+}
+
+
+static async Task<ListBucketsResponse> MyListBucketsAsync(IAmazonS3 s3Client)
+{
+    return await s3Client.ListBucketsAsync();
+}
